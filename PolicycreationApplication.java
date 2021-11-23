e9c0686648674a52141e539659805c2e36a303cd
ananya3399 <36256026+ananya3399@users.noreply.github.com>
2021-11-23 12:22:23 +0530

    Create PolicycreationApplication.java

---
+++ PolicycreationApplication.java
@@ -0,0 +1,13 @@
+package com.example.policycreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example.policycreation"})
+public class PolicycreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicycreationApplication.class, args);
+	}
+
+}
