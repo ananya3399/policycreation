33577d707cfbe45207efd2a5739a54d41816d690
ananya3399 <36256026+ananya3399@users.noreply.github.com>
2021-11-23 12:18:43 +0530

    Create CustomUserDetails.java

---
+++ security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.policycreation.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Autowired
+    public CustomUserDetails(User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserpassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
