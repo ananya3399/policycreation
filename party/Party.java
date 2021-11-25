b00f5e112fd77447a6539f335635ef6394c3d698
ananya3399 <36256026+ananya3399@users.noreply.github.com>
2021-11-25 11:13:44 +0530

    Update Party.java

--- party/Party.java
+++ party/Party.java
@@ -1,13 +1,11 @@
 package com.example.policycreation.party;
 
-import lombok.*;
-
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "party")
 public class Party {
@@ -30,18 +28,128 @@ public class Party {
     private String lastname;
 
     @Column(name = "birthdate")
-    private String birthdate;
+    private LocalDate birthdate;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @Transient
+    @Min(value = 18, message = "Age under 18")
+    private int age;
+
+    @OneToMany(targetEntity = Address.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "partyid", referencedColumnName = "partyid")
     private List<Address> addresses;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "partyid", referencedColumnName = "partyid")
     private List<ContactChannel> contactChannels;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "partyid", referencedColumnName = "partyid")
     private List<Roles> roles;
 
+    public Party() {
+        super();
+    }
+
+    public Party(int partyid, int accountmanagernumber, String gender, String firstname, String lastname, LocalDate birthdate, List<Address> addresses, List<ContactChannel> contactChannels, List<Roles> roles) {
+        this.partyid = partyid;
+        this.accountmanagernumber = accountmanagernumber;
+        this.gender = gender;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.addresses = addresses;
+        this.contactChannels = contactChannels;
+        this.roles = roles;
+    }
+
+    public int getPartyid() {
+        return partyid;
+    }
+
+    public void setPartyid(int partyid) {
+        this.partyid = partyid;
+    }
+
+    public int getAccountmanagernumber() {
+        return accountmanagernumber;
+    }
+
+    public void setAccountmanagernumber(int accountmanagernumber) {
+        this.accountmanagernumber = accountmanagernumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        LocalDate now = LocalDate.now();
+        Period period = Period.between(birthdate, now);
+        Integer gap = Math.abs(period.getYears());
+        this.age=gap;
+        this.birthdate = birthdate;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<ContactChannel> getContactChannels() {
+        return contactChannels;
+    }
+
+    public void setContactChannels(List<ContactChannel> contactChannels) {
+        this.contactChannels = contactChannels;
+    }
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Roles> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "Party{" +
+                "partyid=" + partyid +
+                ", accountmanagernumber=" + accountmanagernumber +
+                ", gender='" + gender + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birthdate=" + birthdate +
+                ", addresses=" + addresses +
+                ", contactChannels=" + contactChannels +
+                ", roles=" + roles +
+                '}';
+    }
 }
