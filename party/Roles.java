a057022937ee1f5640bc8ba97089ba7737f897a1
ananya3399 <36256026+ananya3399@users.noreply.github.com>
2021-11-25 11:18:54 +0530

    Update Roles.java

--- party/Roles.java
+++ party/Roles.java
@@ -1,12 +1,7 @@
 package com.example.policycreation.party;
 
-import lombok.*;
-
 import javax.persistence.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Roles {
@@ -18,4 +13,37 @@ public class Roles {
 
     @Column(name = "rolename")
     private String rolename;
+
+    public Roles() {
+        super();
+    }
+
+    public Roles(int roleid, String rolename) {
+        this.roleid = roleid;
+        this.rolename = rolename;
+    }
+
+    public int getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(int roleid) {
+        this.roleid = roleid;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+
+    @Override
+    public String toString() {
+        return "Roles{" +
+                "roleid=" + roleid +
+                ", rolename='" + rolename + '\'' +
+                '}';
+    }
 }
